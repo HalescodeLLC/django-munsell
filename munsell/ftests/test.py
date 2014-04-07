9f3f26f436bf670a8fbe8ca1ab8ad1b438d0727b
peter_stratton <peter.stratton@halescode.com>
2014-04-07 16:42:21 +0000

    first FT started, using unittest for now

---
+++ munsell/ftests/test.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_convert_a_munsell_color(self):
+        # Nina has heard about a cool new online tool that converts Munsell
+        # colors to RGB and HEX values.  She goes to the homepage:
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention Munsell colors
+        assert 'Munsell' in self.browser.title
+
+        # Satisfied, she goes back to sleep
+        self.browser.quit()
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
