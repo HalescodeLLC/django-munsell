127d0a7ed359131e3c60a203deca3411ffdd051f
peter_stratton <peter.stratton@halescode.com>
2014-04-07 18:03:19 +0000

    ftests now check if we can input a munsell color

--- munsell/ftests/test.py
+++ munsell/ftests/test.py
@@ -1,4 +1,20 @@
+## HueA - 5
+## HueB - YR
+## Value - 4
+## Chroma - 6
+## Nice_Name - Yellowish Red
+## Munsell_Name - 5YR 4/6
+## Sortable_Name - 05YR 4/6
+## R - 0.5333
+## G - 0.3255
+## B - 0.1843
+## dR - 136
+## dG - 83
+## dB - 47
+## Hex - 88532F
+
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -18,6 +34,24 @@ class NewVisitorTest(unittest.TestCase):
 
         # She notices the page title and header mention Munsell colors
         assert 'Munsell' in self.browser.title
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Munsell', header_text)
+
+        # She is invited to enter a munsell color straight away
+        inputbox = self.browser.find_element_by_id('id_munsell_entry')
+        self.assertEqual(inputbox.get_attribute('placeholder'),
+                         'Enter a munsell color'
+                         )
+
+        # She types '5YR 4/6'
+        inputbox.send_keys('5YR 4/6')
+
+        # When she hits enter, the page updates, and now the RGB values are
+        # listed for her color
+        inputbox.send_keys(Keys.ENTER)
+
+        chips = self.browser.find_elements_by_class_name('id_color_chip')
+        self.assertTrue(any(chips.text == '136 83 47' for chip in chips))
 
         # Satisfied, she goes back to sleep
         self.fail("Finish the ftests.")
