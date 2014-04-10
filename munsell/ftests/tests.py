f075d2de7f1b22ec7b24d18c2023e0773e9e3b63
peter_stratton <peter.stratton@halescode.com>
2014-04-10 17:36:12 +0000

    Added basic test to make sure style sheet is being applied

--- munsell/ftests/tests.py
+++ munsell/ftests/tests.py
@@ -75,3 +75,13 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_correct_number_of_results(10)
 
         # Satisfied, she goes back to sleep
+
+    def test_layout_and_styling(self):
+
+        # Nina goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_munsell_entry')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=3)
