aa7aec4897b9f5a7e5e1cf439d41d6928df29e3b
peter_stratton <peter.stratton@halescode.com>
2014-04-11 15:16:39 +0000

    added a parenthesis that had mysteriously disappeared

--- munsell/mcolor/views.py
+++ munsell/mcolor/views.py
@@ -8,7 +8,7 @@ from .models import MunsellColor
 def home_page(request):
     if request.method == 'POST':
         if '_exact_match' in request.POST:
-            colors = MunsellColor.objects.filter(munsell_name=request.POST['munsell_color'].upper()
+            colors = MunsellColor.objects.filter(munsell_name=request.POST['munsell_color'].upper())
             return render(request, 'mcolor/home.html', {'colors': colors})
         else:
             colors = MunsellColor.objects.filter(sortable_name__icontains=request.POST['munsell_color'].replace(' ',''))
